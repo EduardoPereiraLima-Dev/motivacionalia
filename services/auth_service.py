dcacbb01438f4fe76cea977ed4d474fc4296c5b1
Eduardo Pereira <eduardopereira.lima@yahoo.com>
2025-06-03 15:03:21 -0300

    atualização do da autenticação

--- services/auth_service.py
+++ services/auth_service.py
@@ -1,26 +1,29 @@
 from fastapi import Depends, HTTPException, status
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from firebase_admin import auth, credentials, initialize_app
+from dotenv import load_dotenv
 import os
-import json
+
+# Carrega variáveis do .env
+load_dotenv()
 
 security = HTTPBearer(auto_error=False)
 
-# Carrega JSON das credenciais da variável de ambiente e inicializa o Firebase
-firebase_credentials_json = os.getenv("FIREBASE_CREDENTIALS_JSON")
-if not firebase_credentials_json:
-    raise RuntimeError("Variável FIREBASE_CREDENTIALS_JSON não configurada")
+# Carrega o caminho do arquivo de credenciais da variável de ambiente e inicializa o Firebase
+firebase_credentials_path = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
+if not firebase_credentials_path:
+    raise RuntimeError("Variável GOOGLE_APPLICATION_CREDENTIALS não configurada")
 
-cred_dict = json.loads(firebase_credentials_json)
-cred = credentials.Certificate(cred_dict)
+cred = credentials.Certificate(firebase_credentials_path)
 initialize_app(cred)
 
+# Função para validar usuário autenticado via token do Firebase
 async def get_firebase_user(credentials: HTTPAuthorizationCredentials = Depends(security)):
     if not credentials:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Bearer authentication required",
-            headers={"WWW-Authenticate": 'Bearer realm="auth_required"'},
+            headers={"WWW-Authenticate": 'Bearer realm=\"auth_required\"'},
         )
     try:
         decoded_token = auth.verify_id_token(credentials.credentials)
@@ -28,12 +31,12 @@ async def get_firebase_user(credentials: HTTPAuthorizationCredentials = Depends(
             raise HTTPException(
                 status_code=status.HTTP_401_UNAUTHORIZED,
                 detail="Authentication must be via Google account",
-                headers={"WWW-Authenticate": 'Bearer error="invalid_token"'},
+                headers={"WWW-Authenticate": 'Bearer error=\"invalid_token\"'},
             )
         return decoded_token
     except Exception as e:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail=f"Invalid authentication credentials: {str(e)}",
-            headers={"WWW-Authenticate": 'Bearer error="invalid_token"'},
+            headers={"WWW-Authenticate": 'Bearer error=\"invalid_token\"'},
         )
